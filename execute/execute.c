238d754f93d46c8a27c02c47394c18385df7a1a7
sideseal <gychoi@student.42seoul.kr>
2023-02-23 19:14:42 +0900

    fixing conflict

--- execute/execute.c
+++ execute/execute.c
@@ -12,6 +12,8 @@
 
 #include "../include/execute.h"
 
+pid_t temp[2];
+
 int	execute_builtin(t_cmd *node, t_env *environ, int process_type)
 {
 	int	ret;
@@ -68,14 +70,28 @@ void	pipeline_child(t_cmd *node, t_env *environ)
 	int		pfd[2];
 	pid_t	pid;
 
+	// if (node->fd_in != -2)
+	// 	dup2(node->fd_in, STDIN_FILENO);
+	// else
+	// 	node->fd_in = dup(STDIN_FILENO);
+	// if (node->fd_out != -2)
+	// 	dup2(node->fd_out, STDOUT_FILENO);
+	// else
+	// 	node->fd_out = dup(STDOUT_FILENO);
+	
 	ft_pipe(pfd);
 	pid = fork();
+	temp[0] = pid;
 	if (pid == -1)
 		execute_error("failed to fork", CHILD);
 	else if (pid == 0)
 	{
+		if (node->fd_in != -2)
+			ft_dup2(node->fd_in, STDIN_FILENO, CHILD);
 		ft_close(pfd[READ_END], CHILD);
 		ft_dup2(pfd[WRITE_END], STDOUT_FILENO, CHILD);
+		if (node->fd_out != -2)
+			ft_dup2(node->fd_out, STDOUT_FILENO, CHILD);
 		if (execute_command_type(node, environ, CHILD) == 1)
 			command_not_found(node->cmd);
 		else
@@ -83,8 +99,6 @@ void	pipeline_child(t_cmd *node, t_env *environ)
 	}
 	else
 	{
-		if (waitpid(pid, NULL, 0) == -1)
-			execute_error("failed to waitpid", CHILD);
 		ft_close(pfd[WRITE_END], CHILD);
 		ft_dup2(pfd[READ_END], STDIN_FILENO, CHILD);
 	}
@@ -101,13 +115,21 @@ void	pipeline(t_cmd *node, t_env *environ)
 		pipeline_child(cur, environ);
 		cur = cur->next;
 	}
-	//set_pipeline_fd(cur);
-	if (execute_command_type(cur, environ, CHILD) == 1)
+	// fork
+	// if child
+	if (cur->fd_in != -2)
+		ft_dup2(cur->fd_in, STDIN_FILENO, PARENT);
+	if (cur->fd_out != -2)
+		ft_dup2(cur->fd_out, STDOUT_FILENO, PARENT);
+	if (execute_command_type(cur, environ, PARENT) == 1)
 		command_not_found(cur->cmd);
 	else
 		exit(0);
-}
 
+	// if parent
+	waitpid(temp[0], NULL, 0);	
+}
+#include <stdio.h>
 int	execute(t_cmd *line, t_env *environ)
 {
 	t_cmd	*node;
@@ -124,14 +146,7 @@ int	execute(t_cmd *line, t_env *environ)
 	}
 	else
 	{
-		pid = fork();
-		if (pid == -1)
-			execute_error("failed to fork", PARENT);
-		else if (pid == 0)
-			pipeline(node, environ);
-		else
-			if (waitpid(pid, NULL, 0) == -1)
-				execute_error("failed to waitpid", PARENT);
+		pipeline(node, environ);
 	}
 	return (0);
 }
